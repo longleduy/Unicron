3ed281d8d764c7208244995d1b45ee6763e63a55
LongLD <longdd21081994@gmail.com>
2017-07-31 15:48:20 +0700

    passport.js and user.js

---
+++ config/passport.js
@@ -0,0 +1,11 @@
+var passport=require('passport');
+var LocalStrategy=require('passport-local').Strategy;
+var User=require('../models/user');
+
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+})
+passport.deserializeUser(function(id,done){
+    User.findById('')
+})
+
--- models/user.js
+++ models/user.js
@@ -8,9 +8,11 @@ var userSchema = mongoose.Schema({
     email: String,
     avatar: String
 })
+var User=mongoose.model('users',userSchema);
 userSchema.methods.HashPass = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
 }
 userSchema.methods.validPass = function (password) {
     return bcrypt.compareSync(password, this.password);
 }
+module.exports=User;
